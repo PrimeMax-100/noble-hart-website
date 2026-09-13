3c6dc2287a76992263a3be570106baf3ae786121
PrimeMax-100 <abdulmajeedabdussalam798@gmail.com>
2026-09-13 02:01:22 +0100

    Delete Noble_Hart_Website directory

--- Noble_Hart_Website/emailjs-config.js
+++
@@ -1,8 +0,0 @@
-// Noble Hart EmailJS configuration
-// Public Key is intended for browser-side EmailJS use.
-window.NOBLE_HART_EMAILJS = {
-  publicKey: "E9fLM8n_lLtdqkuDI",
-  serviceId: "nobhhrt-serv_vqk7qyn",
-  contactTemplateId: "contct_temp-xq0jqam1",
-  newsletterTemplateId: "newlt_temp-xq0jqam2"
-};
